0aa10e63f3c733baee8418085db34deded97294f
hjuuujh <hjuuujh@gmail.com>
2024-08-13 20:21:21 +0900

    project msa base

---
+++ init/init.sql
@@ -0,0 +1,6 @@
+create database `member`;
+GRANT ALL ON `member`.* TO 'root'@'%';
+create database `store`;
+GRANT ALL ON `store`.* TO 'root'@'%';
+create database `order`;
+GRANT ALL ON `order`.* TO 'root'@'%';
